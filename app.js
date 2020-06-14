bff868420a09f2a4ed86fcfc135f85e36f0d4c16
MuzaffarHussain <zaffarhussain1997122@gmail.com>
2020-06-14 18:13:13 +0500

    Assignment 5 of Javascript is now ready for commit

---
+++ app.js
@@ -0,0 +1,212 @@
+// Chapter Number 1 # Task1
+// 1. Write a script to greet your website visitor using JS alert box
+// alert("Assalam-o-Alykum");
+
+
+
+// Chapter Number 1 # Task2
+// 2. Write a script to display following message on your web page: 
+// alert("Error! Please enter a valid password");
+
+
+
+// Chapter Number 1 # Task3
+// 3. Write a script to display following message on your web page:
+//(Hint : Use line break)
+// alert("Welcome to JS Land.. \n Happy Coding!");
+
+
+
+// Chapter Number 1 # Task4
+// 4. Write a script to display following messages in sequence:
+// alert("Welcome to JS Land");
+// alert("Happy Coding!");
+
+
+
+// Chapter Number 1 # Task5
+// 5. Generate the following message through browser’s
+// developer console
+// console.log("Hello...I can run JS through my web browser's console");
+
+//===========================================================================================================================================================================================================================
+
+// Chapter Number 2 # Task1
+// 1. Declare a variable called username.
+// var username;
+
+
+
+
+
+// Chapter Number 2 # Task2
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+// var myName="Muzaffar Hussain";
+
+
+
+// Chapter Number 2 # Task3
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box
+// var JS;
+// var titledmessage="Hello World";
+// alert(titledmessage);
+
+
+
+
+// Chapter Number 2 # Task4
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+// var JS=["Jhone Doe","15 years old","Certified Mobile Application Development"];
+// for(i=0;i<=JS.length;i++)
+// alert(JS[i]);
+
+
+
+
+
+// Chapter Number 2 # Task5
+// 5. Write a script to display the following alert using one JS
+// variable:
+// var JS=["PIZZA","PIZZ","PIZ","PI","P"];
+// alert(JS[0]+"\n"+JS[1]+"\n"+JS[2]+"\n"+JS[3]+"\n"+JS[4]);
+ 
+// Second Method
+// var JS=["PIZZA","PIZZ","PIZ","PI","P"];
+// for(i=0;i<=JS.length;i++)
+// alert(JS[i]);
+
+
+
+
+
+// Chapter Number 2 # Task6
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+// var email="example@example.com";
+// alert("My email Address is "+email);
+
+
+
+
+
+// Chapter Number 2 # Task7
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+// var book="A smarter way to learn JavaScript";
+// alert("I am trying to learn from the book "+ book);
+
+
+
+// Chapter Number 2 # Task8
+// 8. Write a script to display this in browser through JS
+// document.write("Yah!, I can write HTML content through JavaScript");
+
+
+// Chapter Number 2 # Task9
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+// var deSIGn="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(deSIGn);
+
+
+//===========================================================================================================================================================================================================================
+// Chapter Number 3 # Task1
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+// var age="22";
+// alert("I am "+age+" years old");
+
+
+
+
+
+// Chapter Number 3 # Task2
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+// var N;
+// alert("You have visited this ");
+
+
+
+
+
+
+
+
+
+// Chapter Number 3 # Task3
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+// var birthYear="1998";
+// document.write("My birth day is "+birthYear+"<br>"+"Data type of my declared variable is number");
+
+
+
+
+
+
+// Chapter Number 3 # Task4
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// var name="Doe";
+// var product="T-shirt(s)";
+// var Quantity="5";
+// document.write(name+" ordered "+Quantity+" "+product+" on XYZ Clothing store”");
+
+
+
+
+
+
+
+
+
+// Chapter Number 4 # Task1
+// 1. Declare 3 variables in one statement.
+// 2. Declare 5 legal & 5 illegal variable names.
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+// var head="Rules for naming JS variables";
+// var stVariable=["number",$,_];
+// var ndVariable=["letter",$,_];
+// var rdVariable="sensitive";
+// var thVariable="Keywords";
+// document.write(head);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
